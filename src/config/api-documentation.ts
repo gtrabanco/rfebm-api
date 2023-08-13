5be339f49663fc4cb39a8c527a5c084fc8c07106
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2023-08-14 00:06:44 +0200

    Solve documentation problem in production defineing servers

--- src/config/api-documentation.ts
+++ src/config/api-documentation.ts
@@ -1,12 +1,25 @@
 import { config } from ".";
 import { API_DESCRIPTION } from "./api-description";
 
+const servers = [];
+if (process.env.NODE_ENV === "development") {
+  servers.push({
+    url: `http://${config.serve!.hostname}:${config.serve!.port}`,
+    description: "Local server",
+  });
+}
+
+servers.push({
+  url: `https://rfbem.7m.es`,
+  description: "Production server",
+});
+
 export const API_DOCUMENTATION = {
   info: {
     title: "RFEBM API",
     description: API_DESCRIPTION,
     version: "1.0.0",
-    termsOfService: "http://rfbem.7m.es/terms",
+    termsOfService: "https://rfbem.7m.es/terms",
     contact: {
       name: "Gabriel Trabanco",
       email: "gtrabanco@fwok.org",
@@ -17,14 +30,5 @@ export const API_DOCUMENTATION = {
       url: "/license",
     },
   },
-  servers: [
-    {
-      url: `http://${config.serve!.hostname}:${config.serve!.port}`,
-      description: "Local server",
-    },
-    {
-      url: `http://rfbem.7m.es`,
-      description: "Production server",
-    },
-  ],
+  servers,
 };
