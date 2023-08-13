0d6521f5a80c51d09ca4125d61aed44178e2f481
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2023-08-13 22:28:04 +0200

    Added redirect

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,5 +3,15 @@ import { apiv1 } from "./v1/routes";
 
 export default (app: Elysia) =>
   app
+    .state("latest", "v1")
     .group("/v1", (app) => app.use(apiv1))
-    .group("/latest", (app) => app.use(apiv1));
+    .group("/latest", (app) =>
+      app
+        .route("ALL", "", ({ set, store: { latest } }) => {
+          set.redirect = `/api/${latest}`;
+        })
+        .route("ALL", "*", ({ params, set, store: { latest } }) => {
+          const apiRoute = (params as Record<string, string>)["*"];
+          set.redirect = `/api/${latest}/${apiRoute}`;
+        }),
+    );
