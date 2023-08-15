55805b6a0ea1fd486eed1e4411cebe1c629a2ba0
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2023-08-16 00:19:51 +0200

    Added rate limits information to the documentation

--- src/config/api-description.ts
+++ src/config/api-description.ts
@@ -2,12 +2,15 @@ export const API_DESCRIPTION = `REST API to get data from RFEBM (Real Federació
 
 **This rest api could be used but can be limited requests api in the future. So, do your staff trying to save requests.**
 
+**Rate Limits**
+To avoid massive request to official webpage and optimize the performance of this api, the requests are limited to 10 request per minute to the same endpoint.
+
 **IMPORTANT**
-All non known routes will be redirected to the documentation
+All non known routes will be redirected to the documentation.
 
 **DISCLAIMER**
 This api is provided as is without any warranty. The author is not responsible for any damage caused by the use of this api.
 
 **ABOUT THE USAGE**
-This api is currently under development. So, the usage could be changed in the future. If you want to use this api, use it but be advised. Remember there is no warranty of any damage that was caused by the usage of this api to your application.
+This api is currently under development. So, the usage could be changed in the future. If you want to use this api, use it but be advised.
 `;
