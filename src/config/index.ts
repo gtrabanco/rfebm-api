d1299b5c4da5c6a311fece3efa3164ae1e194d18
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2023-08-18 15:12:19 +0200

    Different rate limits

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,13 +10,35 @@ export const config: ElysiaConfig & { rateLimit: any } = {
     development: !process.env.NODE_ENV?.toLowerCase().startsWith("prod"),
   } as Partial<Serve>,
   rateLimit: {
-    duration: 60000,
-    max: 10,
-    responseCode: 429,
-    responseMessage: "Rate limit exceeded, retry in 15 seconds.",
-    countFailedRequest: false,
-    skip: (request: Request) =>
-      request.url.toString().includes("/live") ||
-      !process.env.NODE_ENV?.toLowerCase().startsWith("prod"), // Avoid on development, which mean avoid cache if non on production
+    general: {
+      duration: 60000,
+      max: 10,
+      responseCode: 429,
+      responseMessage: "Rate limit exceeded, retry in 15 seconds.",
+      countFailedRequest: false,
+      skip: (request: Request) =>
+        request.url.toString().endsWith("/live") ||
+        !process.env.NODE_ENV?.toLowerCase().startsWith("prod"), // Avoid on development, which mean avoid cache if non on production
+    },
+    live: {
+      duration: 15000,
+      max: 2,
+      responseCode: 429,
+      responseMessage: "Rate limit exceeded, retry in 15 seconds.",
+      countFailedRequest: false,
+      skip: (request: Request) =>
+        !request.url.toString().endsWith("/live") ||
+        !process.env.NODE_ENV?.toLowerCase().startsWith("prod"), // Avoid on development, which mean avoid cache if non on production
+    }, // live are different from stream, stream are SSE endpoints
+    // stream: {
+    //   duration: 5000,
+    //   max: 10,
+    //   responseCode: 429,
+    //   responseMessage: "Rate limit exceeded, retry in 15 seconds.",
+    //   countFailedRequest: false,
+    //   skip: (request: Request) =>
+    //     !request.url.toString().endsWith("/stream") ||
+    //     !process.env.NODE_ENV?.toLowerCase().startsWith("prod"), // Avoid on development, which mean avoid cache if non on production
+    // }, // SSE endpoints this a TODO
   },
 };
