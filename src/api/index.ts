ad1979ea26d703939c3573976f9ecfae64977e5a
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2023-09-21 02:51:56 +0200

    Remove v1 but keep files

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,10 @@
 import { type Context, Elysia } from "elysia";
 import { apiv2 } from "./v2/routes";
-import { apiv1 } from "./v1/routes";
+// import { apiv1 } from "./v1/routes";
 
 export default (app: Elysia) =>
   app
-    .use(apiv1)
+    // .use(apiv1)
     .state("latest", "v2")
     .group("/v2", (app) => app.use(apiv2))
     .group("/latest", (app) =>
