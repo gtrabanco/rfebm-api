439856c503594dbb06bc2779ae85571faac4f18c
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2023-09-21 02:53:26 +0200

    fix: remove import of non existent file

--- src/api/v2/routes/tournament/index.ts
+++ src/api/v2/routes/tournament/index.ts
@@ -1,10 +1,9 @@
 import type { Elysia } from "elysia";
 import MatchIdLive from "./match/[matchId]-live";
 import team from "./team";
-import tournament from "./tournament";
 import tournamentWeek from "./tournament-week";
 
 export default (app: Elysia) =>
   app.group("/tournament", (app) =>
-    app.use(tournamentWeek).use(tournament).use(team).use(MatchIdLive),
+    app.use(tournamentWeek).use(team).use(MatchIdLive),
   );
